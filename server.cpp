871fe06a0bb133f92508b4342092c0e7b4863b8f
Zakaria El Kachradi <zel-kach@e2r2p2.1337.ma>
2024-02-29 23:31:33 +0100

    error checking

--- server.cpp
+++ server.cpp
@@ -1,5 +1,4 @@
 #include "server.hpp"
-#include <poll.h>
 
 server_c::server_c() {
 }
@@ -32,18 +31,42 @@ int server_c::getServer_socket() const {
 }
 
 void    server_c::init_server(std::string tmp_port, std::string tmp_password) {
+    int     tmp_client_socket;
+    int     non_block;
+    int     tmp_socket;
+    ssize_t bytes;
+
     setPort(tmp_port);
     setPassword(tmp_password);
 
-    setServer_socket(socket(AF_INET, SOCK_STREAM, 0));
+    #ifdef COMMENTED_OUT
+        socket() params.
+            PARAM1 domain:
+                specifies communication domain. We use AF_ LOCAL as defined in the POSIX standard for communication between processes on the same host.
+                For communicating between processes on different hosts connected by IPV4, we use AF_INET and AF_I NET 6 for processes connected by IPV6.
+            PARAM2 type:
+                SOCK_STREAM == TCP. as specified in subject.
+            PARAM3 protocol:
+                if set to 0 the operating system will choose the default protocol based on the specified address family (AF_INET) and socket type (SOCK_STREAM).
+                insted ill expicitly specify it by using (IPPROTO_TCP).
+    #endif
 
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(getPort());
+    tmp_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (tmp_socket == -1)
+        return std::cerr << "Error: socket" << std::endl, (void)NULL;
 
-    bind(getServer_socket(), (struct sockaddr*)&server_addr, sizeof(server_addr));
-    listen(getServer_socket(), SOMAXCONN);
+    setServer_socket(tmp_socket);
+
+    struct sockaddr_in sockAddr;
+    sockAddr.sin_family = AF_INET;
+    sockAddr.sin_addr.s_addr = INADDR_ANY;
+    sockAddr.sin_port = htons(getPort());
+
+    if (bind(getServer_socket(), (struct sockaddr*)&sockAddr, sizeof(sockAddr)) == -1)
+        return std::cerr << "Error: bind" << std::endl, (void)NULL;
+
+    if (listen(getServer_socket(), SOMAXCONN) == -1)
+        return std::cerr << "Error: listen" << std::endl, (void)NULL;
 
     client_c::_disc.push_back(pollfd());
     client_c::_disc[0].fd = getServer_socket();
@@ -51,51 +74,53 @@ void    server_c::init_server(std::string tmp_port, std::string tmp_password) {
     client_c::_disc[0].revents = 0;
 
     while (true) {
-        if (poll(client_c::_disc.data(), client_c::_disc.size(), -1) == -1) {
-            std::cerr << "Error: poll" << std::endl;
-            break ;
-        }
+        non_block = poll(client_c::_disc.data(), client_c::_disc.size(), 0);
+        if (non_block == -1)
+            return std::cerr << "Error: poll" << std::endl, (void)NULL;
+        if (non_block == 0)
+            continue ;
         for (size_t i = 0; i < client_c::_disc.size(); ++i) {
             if (client_c::_disc[i].revents) {
                 if (client_c::_disc[i].fd == getServer_socket()) {
-                    int tmp_socket = accept(getServer_socket(), NULL, NULL);
+                    tmp_client_socket = accept(getServer_socket(), NULL, NULL);
+                    if (tmp_client_socket == -1)
+                        return std::cerr << "Error: accept" << std::endl, (void)NULL;
 
                     client_c::_disc.push_back(pollfd());
-                    client_c::_disc.back().fd = tmp_socket;
+                    client_c::_disc.back().fd = tmp_client_socket;
                     client_c::_disc.back().events = POLLIN;
                     client_c::_disc.back().revents = 0;
 
-                    std::cout << "New socket #" << tmp_socket << std::endl;
+                    std::cout << "New socket #" << tmp_client_socket << std::endl;
                 }
                 else {
                     try {
-                        char buffer[1024];
-                        ssize_t bytes = recv(client_c::_disc[i].fd, buffer, sizeof(buffer), 0);
-
+                        char    buffer[1024];
+                        bytes = recv(client_c::_disc[i].fd, buffer, 1024, 0);
+                        if (bytes == -1)
+                            return std::cerr << "Error: recv" << std::endl, (void)NULL;
                         buffer[bytes] = '\0';
-                        if (bytes > 1) {
-                            std::cout << "data from socket #" << client_c::_disc[i].fd << ": " << buffer << std::endl;
+                        if (bytes >= 1) {
+                            std::cout << "data from socket #" << client_c::_disc[i].fd << ": " << buffer;
                             server_c::pars_cmd(buffer, client_c::_disc[i].fd);
                         }
                         else if (bytes == 0) {
-                            close(client_c::_disc[i].fd);
+                            if (close(client_c::_disc[i].fd) == -1)
+                                return std::cerr << "Error: close" << std::endl, (void)NULL;
                             std::cout << "Socket #" << client_c::_disc[i].fd << " closed!" << std::endl;
                             if (clients_map.find(client_c::_disc[i].fd) != clients_map.end())
                                 clients_map.erase(client_c::_disc[i].fd);
                             client_c::_disc.erase(client_c::_disc.begin() + i);
                         }
-                        else if (bytes == -1) {
-                            std::cerr << "Error: recv" << std::endl;
-                            break ;
-                        }
+                        std::cout << std::flush;
                     }
                     catch (...) {}
                 }
             }
         }
     }
-    close(getServer_socket());
-    exit (1);
+    if (close(getServer_socket()) == -1)
+        std::cerr << "Error: close" << std::endl;
 }
 
 void    server_c::pars_cmd(std::string buffer, int client_socket) {
