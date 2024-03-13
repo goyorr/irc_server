2e8727f914caccbe05b726d7053ac952994aa910
Zakaria El Kachradi <zel-kach@e1r6p13.1337.ma>
2024-03-13 02:29:40 +0000

    join and priv

--- srv_cmds.cpp
+++ srv_cmds.cpp
@@ -72,7 +72,7 @@ void    server_c::pars_cmd(const std::string &buffer, const uint16_t &client_soc
                 reg_pass(buffer, client_socket);
         }
         if (clients_map[client_socket].getAuth() && clients_map[client_socket].getRegNick() && clients_map[client_socket].getRegUser()) {
-            std::cout << "socket #" << client_socket << " authenticated successfully." << std::endl;
+            std::cout << clients_map[client_socket].getClient_nick()  << " authenticated successfully." << std::endl;
             std::string message = "001 " + clients_map[client_socket].getClient_nick() + " :Welcome to the ft_irc Network\n";
             if (send(client_socket, message.c_str(),  message.size(), 0) == -1) {
                 std::cerr << "Error: send." << std::endl;
